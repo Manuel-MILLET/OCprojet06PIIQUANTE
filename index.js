3d01768ffa81e221dd89d53f60ef7307f1ff2809
Manuel MILLET <manuel.millet.ovh@gmail.com>
2022-09-26 18:35:04 +0200

    enregistrement email et password OK dans mongoose

--- index.js
+++ index.js
@@ -1,24 +1,59 @@
 /* Projet n°6 "PIIQUANTE"
-*  Cours OpenClassrooms ""
-*  par Manuel MILLET le 25 septembre 2022 à 21h00
+*  Cours OpenClassrooms
+*  par Manuel MILLET le 26 septembre 2022 à 12h00
+*   fichier index.js
+*
 */
 // Initialisation des constantes
+const port = 3000;
 const express = require('express');
 const app = express();
-const port = 3000;
 const cors = require('cors');
+
+// Data Base Mongoose
+const mongoose = require('mongoose');
+const uri = 'mongodb+srv://manuel:manu3409@cluster0.iwisneb.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose
+  .connect(uri)
+  .then(() => console.log('Successful connection to the Mongoose database !!'))
+  .catch((err) => console.error('Error connecting to DataBase: ', err));
+
+// schema
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = mongoose.model('User', userSchema);
+//const manuel = new User({name:'manuel', password:3409});
+/*
+manuel
+    .save()
+    .then(res => console.log('l\'utilisateur manuel est bien enregistré !', res))
+    .catch((err) => console.log('l\'utilisateur manuel n\'a pas été enregistré !!', err));
+console.log('Le profil de Manuel est : ', manuel);
+*/
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json());// permet de transformer(parser) les données json du body
 
-//Routes
+// Routes
 app.post("/api/auth/signup", (req, res) => {
     console.log('Signup request:', req.body);
-    res.send("Utilisateur enregistré !");// à tester avec la méthode res.end 
+    const email = req.body.email;
+    const password = req.body.password;
+    console.log({email,password});
+    const user = new User({ email: email, password: password});
+user
+    .save()
+    .then((res) => console.log('USER SIGNUP ok !!'))
+    .catch((err) => console.log('USER NOT SIGNUP'))
+    res.send("Utilisateur enregistré !" );
 });
 app.get('/', (req, res) => {
-    res.send('SERVER OK !!');// à tester avec la méthode .end
+    res.send('Server connection successful !!');
 });
 app.listen(port, () => {
     console.log('Le server de l\'application PIIQUANTE écoute sur le port 3000');
