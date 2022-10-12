3f3b397af894d43d1a6673b46ed95548d37cdcb5
Manuel MILLET <manuel.millet.ovh@gmail.com>
2022-10-12 19:17:23 +0200

    correction des erreurs sur le calcule des likes et dislikes

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,5 +1,5 @@
  /* Projet n°6 cours OpenClassrooms "PIIQUANTE"
-*  par Manuel MILLET le 08 octobre 2022 à 12h00
+*  par Manuel MILLET le 12 octobre 2022 à 19h00
 *  fichier controllers/sauce.js
 */
 const Sauce = require('../models/Sauce');
@@ -10,61 +10,47 @@ exports.likeSauce = (req, res, next) => {
   const sauceId = req.params.id;// identifiant de la sauce likée ou dislikée
   const userId = req.body.userId;// identifiant de l'utilisateur qui veut liker ou disliker
   const like = req.body.like;// valeure du like ( -1, 0 , 1)
-  console.log('DATA LIKE liste:');
-  console.log('l ID de la sauce à LIKER/DISLIKER est : ', sauceId);
-  console.log('l ID de l utilisateur est :', userId);
-  console.log('le like demandé est : est :', like);
-  //console.log('Les LIKES sont à  : ',sauce.likes);
-  //console.log('Les DISLIKES sont à  : ',sauce.dislikes);
-  //console.log('Les userLiked sont à  : ',sauce.usersLiked);
-  //console.log('Les userdisLiked sont à  : ',sauce.usersDisliked);
 
-  if (![-1,0,1].includes(like)) {// ici la valeure du like est # de -1, 0 ou 1
+  if (![-1,0,1].includes(like)) {// ici la valeure du like est différente de -1, 0 ou 1, donc message d'erreur
     console.log('like value not autorised !!!!');
     res.status(401).json({ message : 'like value not autorised !!!!'});
     return;
   }
 
-  if (like === 1) {// Ici l'utilisateur aime "LIKE" la sauce, like = 1
+  if (like === 1) {// Ici l'utilisateur aime "LIKE" la sauce, like ++
     Sauce.updateOne(
       { _id: sauceId },
-      { $inc: { likes: like },  $push: { usersLiked: userId } }
-    )
-    .then((sauce) => res.status(200).json({ message: "Sauce liked !" }))
-    .catch((error) => res.status(500).json({ error }));
-
-  }
-
-  if (like === -1) {// Ici l'utilisateur n'aime pas "DISLIKE" la sauce, like = -1
+      { $inc: { likes: +1 },  $push: { usersLiked: userId } }
+      )
+      .then((sauce) => res.status(200).json({ message: "Sauce likes = likes +1 !" }))
+      .catch((error) => res.status(500).json({ error }));
+  } else if (like === -1) {// Ici l'utilisateur n'aime pas "DISLIKE" la sauce, like --
     Sauce.updateOne(
       { _id: sauceId },
-      { $inc: { likes: like },  $push: { usersDisliked: userId } }
-    )
-    .then((sauce) => res.status(200).json({ message: "Sauce disliked !" }))
-    .catch((error) => res.status(500).json({ error }));
-
-  } else {// Ici l'utilisateur a changé d'avis sur la sauce, like = 0
-   Sauce.findOne({ _id: sauceId })
+      { $inc: { dislikes: +1 },  $push: { usersDisliked: userId } }
+      )
+      .then((sauce) => res.status(200).json({ message: "Sauce dislikes = dislikes +1 !" }))
+      .catch((error) => res.status(500).json({ error }));
+  } else {// Ici l'utilisateur a changé d'avis sur la sauce, like = 0 : il a supprimé un likes ou il a supprimé un dislikes => si Likes était à 3 il passe à 2, ou si dislikes était à 15 il passe à 14
+    Sauce.findOne({ _id: sauceId })
       .then((sauce) => {
-        if (sauce.usersLiked.includes(userId)) {
+        if (sauce.usersLiked.includes(userId)) {// Ici l'utilisateur a changé d'avis sur le like (j'aime), il faut décrementer la valeur de "likes"
           Sauce.updateOne(
             { _id: sauceId },
             { $pull: { usersLiked: userId }, $inc: { likes: -1 } }
-          )
-            .then((sauce) => res.status(200).json({ message: "Sauce disliked !" }))
+            )
+            .then((sauce) => res.status(200).json({ message: "Sauce likes = likes -1 !" }))
             .catch((error) => res.status(500).json({ error }));
-
-        } else if (sauce.usersDisliked.includes(userId)) {// Ici l'utilisateur a changé d'avis sur le dislike (j'aime pas)
+        } else if (sauce.usersDisliked.includes(userId)) {// Ici l'utilisateur a changé d'avis sur le dislike (j'aime pas), il faut décrémenter la valeur de "dislikes"
           Sauce.updateOne(
             { _id: sauceId },
             { $pull: { usersDisliked: userId }, $inc: { dislikes: -1 }}
             )
-            .then((sauce) => res.status(200).json({ message: "Sauce disliked !" }))
+            .then((sauce) => res.status(200).json({ message: "Sauce dislikes = dislikes -1 !" }))
             .catch((error) => res.status(500).json({ error }));
         }
-      })
+        })
       .catch((error) => res.status(401).json({ error }));
-
   }
 };
 
